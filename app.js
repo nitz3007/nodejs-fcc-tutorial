16dfdef92834c2e2fa29a099bce84e58fe946e6b
nitz30 <niharikat80@gmail.com>
2025-04-11 00:46:31 +0530

    modules - special feature

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const names = require('./names');
 const sayHi = require('./utils');
+require('./mind-grenade-module'); //node invokes the module as a function while importing it. Thus addValues fn gets executed.
 
 sayHi(names.name1);
 sayHi(names.name2);
